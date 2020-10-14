dc22ac21bd0a7d10ba97651cc605bc5cbd43b0fc
Jeff Wendling <leterip@gmail.com>
2020-10-14 19:14:17 -0400

    add env vars to disable vector instructions

--- internal/consts/cpu.go
+++ internal/consts/cpu.go
@@ -1,8 +1,17 @@
 package consts
 
-import "golang.org/x/sys/cpu"
+import (
+	"os"
+
+	"golang.org/x/sys/cpu"
+)
 
 var (
-	HasAVX2  = cpu.X86.HasAVX2
-	HasSSE41 = cpu.X86.HasSSE41
+	HasAVX2 = cpu.X86.HasAVX2 &&
+		os.Getenv("BLAKE3_DISABLE_AVX2") == "" &&
+		os.Getenv("BLAKE3_PUREGO") == ""
+
+	HasSSE41 = cpu.X86.HasSSE41 &&
+		os.Getenv("BLAKE3_DISABLE_SSE41") == "" &&
+		os.Getenv("BLAKE3_PUREGO") == ""
 )
