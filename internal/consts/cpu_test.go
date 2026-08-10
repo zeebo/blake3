a5bcdd09c66586e8a34c8cc14a4146b7e223cd58
Bob Du <i@bobdu.cc>
2026-08-10 23:28:06 +0800

    add github actions ci (#36)

---
+++ internal/consts/cpu_test.go
@@ -0,0 +1,24 @@
+package consts
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestRequireAssembly(t *testing.T) {
+	require := os.Getenv("BLAKE3_TEST_REQUIRE_ASM")
+	if require == "" {
+		t.SkipNow()
+	}
+
+	selected := map[string]bool{"avx2": HasAVX2, "sse41": HasSSE41, "neon": HasNEON}
+	for _, name := range strings.Split(require, ",") {
+		switch ok, known := selected[name]; {
+		case !known:
+			t.Errorf("unknown backend %q", name)
+		case !ok:
+			t.Errorf("%s backend not selected", name)
+		}
+	}
+}
