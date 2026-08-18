4ee8a2b13a1615fa3e7da97ffda162e4819e9a3b
BobDu <i@bobdu.cc>
2026-08-18 08:52:52 +0800

    panic in the unreachable stubs instead of forwarding to the pure code
    Signed-off-by: BobDu <i@bobdu.cc>

--- internal/alg/hash/hash_neon/hash_arm64.go
+++ internal/alg/hash/hash_neon/hash_arm64.go
@@ -1,7 +1,4 @@
-//go:build amd64
-// +build amd64
-
-package hash_avx512
+package hash_neon
 
 //go:noescape
 func HashF(input *[8192]byte, length, counter uint64, flags uint32, key *[8]uint32, out *[64]uint32, chain *[8]uint32)
